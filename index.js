496068f7c0758239ae5277a7f305ba00f016c8b0
David Viuche <jviucheroja@uniminuto.edu.co>
2020-07-17 11:00:26 -0500

    dependencias para produccion

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const empleadosRoutes = require('./src/routes/empleadosRoutes');
 const usuariosRoutes = require('./src/routes/usuariosRoutes');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 app.set('port', process.env.PORT || 3000);
 
 app.use('/empleados', empleadosRoutes);
